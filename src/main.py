fe1595d0eefb8dcd72db8b28bd65e08f4b53d4a6
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-06-07 21:57:06 +0300

    Update

--- src/main.py
+++ src/main.py
@@ -14,6 +14,7 @@ BROKER_ADDRESS = sys.argv[1].split(":")[0]
 BROKER_PORT    = int(sys.argv[1].split(":")[1])
 MONGO_ADDRESS  = sys.argv[2].split(":")[0]
 MONGO_PORT = int(sys.argv[2].split(":")[1])
+MQTT_DOMAIN = "cws"
 
 mongo_connection_str = f"mongodb://{MONGO_ADDRESS}:{MONGO_PORT}/"
 mongo_client = pymongo.MongoClient(mongo_connection_str)
@@ -21,25 +22,34 @@ mongo_client = pymongo.MongoClient(mongo_connection_str)
 def on_connect(client : mqtt.Client, userdata, flags, rc):
     if rc == 0:
         print("Connected to MQTT broker")
-        client.subscribe("/cws/#")
+        client.subscribe(f"/{MQTT_DOMAIN}/#")
     else:
         print(f"Failed to connect, return code: {rc}")
 
 def on_message(client, userdata, msg):
     payload = msg.payload.decode()
+    path = msg.topic.split("/")
 
-    top, build_id, room, src_type, sensor = msg.topic.split("/")[1:]
+    if (len(path) != 6 or path[1] != MQTT_DOMAIN):
+        return;
+
+    top, build_id, room_id, type_id, device_id = path[1:]
     data = {}
-    data["room"] = room
-    data["type"] = src_type
-    data["sensor"] = sensor
-    data["payload"] = json.loads(payload)
+
+    try:
+        data["room"] = room_id
+        data["type"] = type_id
+        data["sensor"] = device_id
+        data["payload"] = json.loads(payload)
+    except ValueError as e:
+        print(repr(e))
+        return
 
     database = mongo_client[top]
     collection = database[build_id]
 
-    collection.insert_one(data)
     print(data)
+    collection.insert_one(data)
 
 mqtt_client = mqtt.Client()
 mqtt_client.on_connect = on_connect
