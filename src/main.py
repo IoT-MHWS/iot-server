21cd5dd70dd1411726686ab32ebef1e12fcc73d0
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-06-07 22:14:15 +0300

    Reformat server

--- src/main.py
+++ src/main.py
@@ -1,37 +1,53 @@
 import paho.mqtt.client as mqtt
 import sys
+from collections import namedtuple
 import json
+import logging
 import pymongo
 
-if __name__ != "__main__":
-    exit(0)
-
-if len(sys.argv) != 3:
-    print("Args: <broker_address>:<port> <mongo_address>:<port>")
-    sys.exit(1)
-
-BROKER_ADDRESS = sys.argv[1].split(":")[0]
-BROKER_PORT    = int(sys.argv[1].split(":")[1])
-MONGO_ADDRESS  = sys.argv[2].split(":")[0]
-MONGO_PORT = int(sys.argv[2].split(":")[1])
 MQTT_DOMAIN = "cws"
 
-mongo_connection_str = f"mongodb://{MONGO_ADDRESS}:{MONGO_PORT}/"
-mongo_client = pymongo.MongoClient(mongo_connection_str)
 
-def on_connect(client : mqtt.Client, userdata, flags, rc):
+Socket = namedtuple("Socket", "address port")
+InputArgs = namedtuple("InputArgs", ["broker_socket", "mongo_socket"])
+
+
+mongo_client: pymongo.MongoClient
+
+
+def process_arguments() -> InputArgs:
+    try:
+        broker_address, broker_port = sys.argv[1].split(":")
+        mongo_address, mongo_port = sys.argv[2].split(":")
+    except ValueError:
+        raise ValueError(f"Look in process_argument() for valid argument passing")
+
+    return InputArgs(
+        broker_socket=Socket(address=broker_address, port=int(broker_port)),
+        mongo_socket=Socket(address=mongo_address, port=int(mongo_port)),
+    )
+
+
+def mongo_get_client(address, port):
+    mongo_connection_str = f"mongodb://{address}:{port}/"
+    mongo_client = pymongo.MongoClient(mongo_connection_str)
+    return mongo_client
+
+
+def mqtt_on_connect(client: mqtt.Client, userdata, flags, rc):
     if rc == 0:
         print("Connected to MQTT broker")
         client.subscribe(f"/{MQTT_DOMAIN}/#")
     else:
         print(f"Failed to connect, return code: {rc}")
 
-def on_message(client, userdata, msg):
+
+def mqtt_on_message(client, userdata, msg):
     payload = msg.payload.decode()
     path = msg.topic.split("/")
 
-    if (len(path) != 6 or path[1] != MQTT_DOMAIN):
-        return;
+    if len(path) != 6 or path[1] != MQTT_DOMAIN:
+        return
 
     top, build_id, room_id, type_id, device_id = path[1:]
     data = {}
@@ -48,13 +64,26 @@ def on_message(client, userdata, msg):
     database = mongo_client[top]
     collection = database[build_id]
 
-    print(data)
     collection.insert_one(data)
+    logging.info(data)
 
-mqtt_client = mqtt.Client()
-mqtt_client.on_connect = on_connect
-mqtt_client.on_message = on_message
 
-mqtt_client.connect(BROKER_ADDRESS, BROKER_PORT, keepalive=60)
-mqtt_client.loop_forever()
+def mqtt_connect_loop_forever(address: str, port: int) -> mqtt.Client:
+    mqtt_client = mqtt.Client()
+    mqtt_client.on_connect = mqtt_on_connect
+    mqtt_client.on_message = mqtt_on_message
 
+    mqtt_client.connect(address, port, keepalive=60)
+    mqtt_client.loop_forever()
+    return mqtt_client
+
+
+if __name__ == "__main__":
+    args = process_arguments()
+
+    broker_socket: Socket = args.broker_socket
+    mongo_socket: Socket = args.mongo_socket
+
+    mongo_client = mongo_get_client(mongo_socket.address, mongo_socket.port)
+
+    mqtt_connect_loop_forever(broker_socket.address, broker_socket.port)
