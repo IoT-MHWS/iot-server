dcd3aa733e2a701f8c71fe51b5ec9e45a9b7316f
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-06-07 16:37:07 +0300

    Add mongodb

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import paho.mqtt.client as mqtt
 import sys
+import json
+import pymongo
 
 if __name__ != "__main__":
     exit(0)
 
 if len(sys.argv) != 3:
-    print("Args: <broker_address> <broker_port>")
+    print("Args: <broker_address>:<port> <mongo_address>:<port>")
     sys.exit(1)
 
-sensor_type = "temp"
-broker_address = sys.argv[1]
-broker_port = int(sys.argv[2])
+BROKER_ADDRESS = sys.argv[1].split(":")[0]
+BROKER_PORT    = int(sys.argv[1].split(":")[1])
+MONGO_ADDRESS  = sys.argv[2].split(":")[0]
+MONGO_PORT = int(sys.argv[2].split(":")[1])
+
+mongo_connection_str = f"mongodb://{MONGO_ADDRESS}:{MONGO_PORT}/"
+mongo_client = pymongo.MongoClient(mongo_connection_str)
 
 def on_connect(client : mqtt.Client, userdata, flags, rc):
     if rc == 0:
@@ -21,12 +27,22 @@ def on_connect(client : mqtt.Client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     payload = msg.payload.decode()
-    print(f"From topic '{msg.topic}' received '{payload}'")
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+    top, build_id, room, src_type, sensor = msg.topic.split("/")[1:]
+    data = json.loads(payload)
+    data["room"] = room
+    data["type"] = src_type
+    data["sensor"] = sensor
 
-client.connect(broker_address, broker_port, keepalive=60)
-client.loop_forever()
+    database = mongo_client[top]
+    collection = database[build_id]
+
+    collection.insert_one(data)
+
+mqtt_client = mqtt.Client()
+mqtt_client.on_connect = on_connect
+mqtt_client.on_message = on_message
+
+mqtt_client.connect(BROKER_ADDRESS, BROKER_PORT, keepalive=60)
+mqtt_client.loop_forever()
 
